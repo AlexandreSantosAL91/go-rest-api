a2af52ef29584fb2cd09af3d58a4f004ddbdf6a7
Alexandre Santos <alexandresantos_al@hotmail.com>
2022-11-15 00:33:48 -0300

    Create main.go

---
+++ api/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/users", getUsers)
+	fmt.Println("Iniciando o servidor Rest com Go")
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+	/*
+		Endereço do localhost: http://localhost:8080/users
+	*/
+}
+
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content=Type", "application/json")
+	json.NewEncoder(w).Encode([]User{
+		{
+			ID:   1,
+			Name: "Estrela 1.0",
+		},
+
+		{
+			ID:   1,
+			Name: "Estrela 2.0",
+		},
+		{
+			ID:   3,
+			Name: "Estrela 2.0",
+		},
+	})
+}
